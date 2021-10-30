65166ab008b25bf13b4871879e855fb5707604ee
Nazaralieff <nazaralyeff@gmail.comm>
2021-10-30 14:32:59 +0400

    Works are done!

---
+++ VolumeOfBox/VolumeOfBox/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VolumeOfBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            static int VolueOfBox(int widht, int length, int height)
+            {
+                return widht * length * height;
+            }
+
+            Console.WriteLine(VolueOfBox(length : 2, widht : 2, height : 2));
+        }
+
+    }
+}
