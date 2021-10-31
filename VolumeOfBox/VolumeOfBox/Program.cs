77331a4ad2163fee93ab0974ec83dabab3850e98
Nazaralieff <nazaralyeff@gmail.comm>
2021-10-31 22:30:27 +0400

    Modified

--- VolumeOfBox/VolumeOfBox/Program.cs
+++ VolumeOfBox/VolumeOfBox/Program.cs
@@ -6,14 +6,28 @@ namespace VolumeOfBox
     {
         static void Main(string[] args)
         {
+            Volume vol = new Volume();
+            Console.Write("Enter Width : ");
+            vol.width = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter length : ");
+            vol.lenhtg = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter heigth : ");
+            vol.heigth = Convert.ToInt32(Console.ReadLine());
 
-            static int VolueOfBox(int widht, int length, int height)
+
+            static void VolumeOfBox(Volume obj)
             {
-                return widht * length * height;
+                Console.WriteLine(obj.width * obj.heigth * obj.lenhtg);
             }
 
-            Console.WriteLine(VolueOfBox(length : 2, widht : 2, height : 2));
+            VolumeOfBox(vol);
         }
 
+    }
+    class Volume
+    {
+        public int width;
+        public int lenhtg;
+        public int heigth;
     }
 }
