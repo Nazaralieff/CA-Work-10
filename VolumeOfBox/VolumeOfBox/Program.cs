e31b4e7eb020861db692513f6090a0338b4daa82
Nazaraliyev <tu201806044@aztu.local>
2021-11-01 08:31:10 +0400

    Modified

--- VolumeOfBox/VolumeOfBox/Program.cs
+++ VolumeOfBox/VolumeOfBox/Program.cs
@@ -14,13 +14,12 @@ namespace VolumeOfBox
             Console.Write("Enter heigth : ");
             vol.heigth = Convert.ToInt32(Console.ReadLine());
 
-
-            static void VolumeOfBox(Volume obj)
-            {
-                Console.WriteLine(obj.width * obj.heigth * obj.lenhtg);
-            }
-
             VolumeOfBox(vol);
+        }
+
+        static void VolumeOfBox(Volume obj)
+        {
+            Console.WriteLine(obj.width * obj.heigth * obj.lenhtg);
         }
 
     }
